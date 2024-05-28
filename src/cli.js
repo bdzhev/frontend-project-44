9ccbd7c7ab0242fcfc700083855c6f5a7e28252e
bay <bayeldzhoroev@gmail.com>
2024-05-28 22:31:29 +0300

    Connect game logic to the main script

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 
 import readLineSync from 'readline-sync';
+import brainEven from '../bin/brain-even.js';
 
-const getUserName = () => {
+const brainGame = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readLineSync.question('Your answer: ');
+  const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+
+  const result = brainEven();
+  
+  if (!result) {
+    console.log(`Let's try again, ${name}!`);
+  }
+  console.log(`Congratulations, ${name}!`);
 };
 
-export default getUserName;
+export default brainGame;
