d06ff6fa0932c43e01c8f18759a56e19ac3f3f4e
bay <bayeldzhoroev@gmail.com>
2024-05-29 18:45:12 +0300

    Remove all game logic from src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 
 import readLineSync from 'readline-sync';
-import brainEven from '../bin/brain-even.js';
 
 const brainGame = () => {
   console.log('Welcome to the Brain Games!');
   const name = readLineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-
-  brainEven(name);
+  return name;
 };
 
 export default brainGame;
