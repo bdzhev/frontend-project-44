ce2d2b2dd3187c581e800c45e91635a83d8e2a8a
bay <bayeldzhoroev@gmail.com>
2024-05-29 19:39:28 +0300

    Add main gcd file and logic

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+import { glogic, randomN, findGCD } from '../../src/index.js';
+import getName from '../../src/cli.js';
+
+const name = getName();
+
+console.log('Find the greatest common divisor of given numbers.');
+const maxAttempts = 3;
+const maxNum = 100;
+const minNum = 1;
+
+let j = 0;
+let isAnswerCorrect = true;
+while (j < maxAttempts && isAnswerCorrect === true) {
+  // Generate numbers and the gcd
+  const n1 = randomN(minNum, maxNum);
+  const n2 = randomN(minNum, maxNum);
+
+  const gcd = findGCD(n1, n2).toString();
+
+  // Form an expression
+  const expression = `${n1} ${n2}`;
+
+  // Run the main game logic
+  isAnswerCorrect = glogic(gcd, expression);
+  j += 1;
+}
+
+if (j < maxAttempts) {
+  console.log(`Let's try again, ${name}!`);
+} else {
+  console.log(`Congratulations, ${name}!`);
+}
