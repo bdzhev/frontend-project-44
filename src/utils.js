9e0a3e4e812b7c19a00d1c81e22f1ad42f7b8fe2
bay <bayeldzhoroev@gmail.com>
2024-06-10 20:07:36 +0300

    Fix the genRandomNum function to include 1

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 const genRandomNum = (lowerBound, upperBound) => Math.floor(Math.random()
-* (upperBound - lowerBound) + lowerBound);
+* (upperBound + 1 - lowerBound) + lowerBound);
 
 export default genRandomNum;
